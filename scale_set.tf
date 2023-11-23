83816c9287aca96f87ed20d68621c47d72161064
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-22 21:39:23 -0600

    modified:   network.tf 	modified:   scale_set.tf

--- scale_set.tf
+++ scale_set.tf
@@ -34,7 +34,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vm_ss" {
     primary = true
 
     ip_configuration {
-      name                                   = "subnet_3"
+      name                                   = "IP-Config"
       primary                                = true
       subnet_id                              = azurerm_subnet.subnet_3.id
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.example.id]
