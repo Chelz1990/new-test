191655d27eaf6416bc5a516dc3011258d138a3c4
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-22 18:56:41 -0600

    modified:   network.tf 	modified:   scale_set.tf

--- network.tf
+++ network.tf
@@ -40,7 +40,7 @@ resource "azurerm_network_interface" "example" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.subnet_1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.example.id
   }
