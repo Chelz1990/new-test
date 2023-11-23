6838a1be734d2cf5395eb6467bbca19ee4e4b0e8
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-22 20:59:44 -0600

    modified:   network.tf

--- network.tf
+++ network.tf
@@ -177,7 +177,7 @@ resource "azurerm_lb_rule" "http" {
   backend_port                   = 80
   frontend_ip_configuration_name = "Public-IP"
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.example.id]
-  probe_id                       = [azurerm_lb_probe.http.id]
+  probe_id                       = azurerm_lb_probe.http.id
 }
 
 # Load Balancer - Probe - HTTP
