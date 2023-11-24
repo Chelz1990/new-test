0f1ef255f4861ad2950ce304d690458963add223
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-24 07:37:20 -0600

    modified:   scale_set.tf

--- scale_set.tf
+++ scale_set.tf
@@ -1,5 +1,4 @@
 # ---------------------------------------------------------------------------
-
 # Virtual Machine Scale Set
 resource "azurerm_linux_virtual_machine_scale_set" "vm_ss" {
   name                = "vm-ss"
@@ -11,11 +10,6 @@ resource "azurerm_linux_virtual_machine_scale_set" "vm_ss" {
   admin_password      = "pa$$w0rd"
   custom_data         = filebase64("wordpress.sh")
   health_probe_id     = azurerm_lb_probe.http.id
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   source_image_reference {
     publisher = "OpenLogic"
@@ -30,7 +24,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vm_ss" {
   }
 
   network_interface {
-    name    = "example"
+    name    = "example-nic"
     primary = true
 
     ip_configuration {
