d85b4d32153c284f8e5157412b8fc212904794c7
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-24 09:47:46 -0600

    modified:   scale_set.tf

--- scale_set.tf
+++ scale_set.tf
@@ -1,15 +1,15 @@
 # ---------------------------------------------------------------------------
 # Virtual Machine Scale Set
 resource "azurerm_linux_virtual_machine_scale_set" "vm_ss" {
-  name                = "vm-ss"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.azure-project.name
-  sku                 = "Standard_F2"
-  instances           = 2
-  admin_username      = "adminuser"
-  admin_password      = "pa$$w0rd"
-  custom_data         = filebase64("wordpress.sh")
-  health_probe_id     = azurerm_lb_probe.http.id
+  name                            = "vm-ss"
+  location                        = var.location
+  resource_group_name             = azurerm_resource_group.azure-project.name
+  sku                             = "Standard_F2"
+  instances                       = 2
+  admin_username                  = "adminuser"
+  admin_password                  = "pa$$w0rd"
+  custom_data                     = filebase64("wordpress.sh")
+  health_probe_id                 = azurerm_lb_probe.http.id
   disable_password_authentication = false
 
   source_image_reference {
@@ -25,8 +25,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "vm_ss" {
   }
 
   network_interface {
-    name    = "example"
-    primary = true
+    name                      = "example"
+    primary                   = true
     network_security_group_id = azurerm_network_security_group.nsg.id
 
     ip_configuration {
