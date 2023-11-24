677d019afa68539b2d109bad271b24f33660468e
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-24 09:44:34 -0600

    update

--- network.tf
+++ network.tf
@@ -24,7 +24,7 @@ resource "azurerm_subnet" "subnet_2" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Subnet #3 for Scale Set
+# Subnet #3 for NIC / NSG / Scale Set
 resource "azurerm_subnet" "subnet_3" {
   name                 = "subnet_3"
   virtual_network_name = azurerm_virtual_network.vnet.name
@@ -106,7 +106,7 @@ resource "azurerm_public_ip" "igw_ip" {
   location            = var.location
   resource_group_name = azurerm_resource_group.azure-project.name
 
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
 }
 
 # Internet Gateway
